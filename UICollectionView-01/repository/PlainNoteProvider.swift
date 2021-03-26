275f081f496e4ad8cb1b2cf5592a0835d07fe598
yccheok <yancheng.cheok@gmail.com>
2021-03-26 11:21:09 +0800

    PlainNoteProvider

--- UICollectionView-01/repository/PlainNoteProvider.swift
+++ UICollectionView-01/repository/PlainNoteProvider.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import CoreData
+
+class PlainNoteProvider {
+    
+    weak var fetchedResultsControllerDelegate: NSFetchedResultsControllerDelegate?
+    
+    lazy var fetchedResultsController: NSFetchedResultsController<NSPlainNote> = {
+        
+        // Create a fetch request for the Quake entity sorted by time.
+        let fetchRequest = NSFetchRequest<NSPlainNote>(entityName: "NSPlainNote")
+        fetchRequest.sortDescriptors = [
+            NSSortDescriptor(key: "pinned", ascending: true)
+        ]
+        // Create a fetched results controller and set its fetch request, context, and delegate.
+        let controller = NSFetchedResultsController(fetchRequest: fetchRequest,
+                                                    managedObjectContext: CoreDataStack.INSTANCE.persistentContainer.viewContext,
+                                                    sectionNameKeyPath: "pinned",
+                                                    cacheName: nil
+        )
+        controller.delegate = fetchedResultsControllerDelegate
+        
+        // Perform the fetch.
+        do {
+            try controller.performFetch()
+        } catch {
+            fatalError("Unresolved error \(error)")
+        }
+        
+        return controller
+    }()
+    
+    
+    init(_ fetchedResultsControllerDelegate: NSFetchedResultsControllerDelegate) {
+        self.fetchedResultsControllerDelegate = fetchedResultsControllerDelegate
+    }
+}
