fcd18430958a672b71b752b0183c95a2555fe1b3
yccheok <yancheng.cheok@gmail.com>
2021-04-05 17:32:37 +0800

    WIP

--- UICollectionView-01/Constants.swift
+++ UICollectionView-01/Constants.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+import UIKit
+
+enum Constants {
+    static let DRAG_N_MOVE_ALPHA: CGFloat = 0.8
+    static let DRAG_N_MOVE_SCALE: CGFloat  = 1.02
+    static let DRAG_N_MOVE_SHADOW_OPACITY: Float = 0.8
+    static let DRAG_N_MOVE_SHADOW_RADIUS: CGFloat = 8
+    static let SHADOW_OPACITY: Float = 0.3
+    static let SHADOW_RADIUS: CGFloat = 2
+}
